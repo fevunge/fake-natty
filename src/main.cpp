25a21101980995de3a66b153c7bfb608ed979057
fevunge <developervunge@gmail.com>
2025-11-24 15:22:47 +0100

    Refactor main.cpp to simplify routes and remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,14 @@
 #include <optional>
 
 #define CROW_MAIN
-
 #include "crow.h"
 
-crow::json::wvalue error2json(const std::string &errstring) {
-  crow::json::wvalue r;
-  r["error"] = errstring;
-  return r;
-}
 
 int main() {
   crow::SimpleApp app;
-
-  CROW_ROUTE(app, "/hello").methods(crow::HTTPMethod::Get)
-      ([]() {
-
-        return "hello";
-      });
-
-  CROW_ROUTE(app, "/hello/json").methods(crow::HTTPMethod::Get)
-      ([]() {
-        crow::json::wvalue response;
-        response["msg"] = "hello";
-        return response;
-      });
-
-  CROW_ROUTE(app, "/add/<int>/<int>").methods(crow::HTTPMethod::Get)
-      ([](const crow::request & /*req*/, int a, int b) {
-        int c = a + b;
-        crow::json::wvalue response;
-        response["result"] = c;
-        return response;
-      });
-
-  CROW_ROUTE(app, "/hello/<string>").methods(crow::HTTPMethod::Get)
-      ([](const crow::request & /*req*/, std::string name) {
-
-        crow::json::wvalue response;
-        response["result"] = name;
-        return response;
-      });
-
-  app.port(18080).run();
+  CROW_ROUTE(app, "/")([](){
+    return "fevunge";
+  });
+  app.port(8080).multithreaded();
+  return 0;
 }
