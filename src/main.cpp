cf17fcdb2a14a0cc3bb539f5b4ebf7b51fe51657
fevunge <developervunge@gmail.com>
2025-11-24 15:35:49 +0100

    Fix template path in main.cpp to correctly load the HTML page

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/")([](){
-    auto page = crow::mustache::load_text("template/page.html");
+    auto page = crow::mustache::load_text("templates/page.html");
     return page;
   });
 
